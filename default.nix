c494153e18f4dc1d4dfd93a62dc76c7b20814c8e
burij <michael.burij@gmail.com>
2025-10-07 21:39:07 +0200

    empty envirment

---
+++ default.nix
@@ -0,0 +1,47 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+let
+  appName = "elixir_playground";
+  appVersion = "0.1";
+
+  elixirEnv = with pkgs; [
+    elixir
+    erlang
+  ];
+
+  dependencies = with pkgs; [
+    wget
+    nixpkgs-fmt
+  ];
+
+  shell = pkgs.mkShell {
+    buildInputs = [ elixirEnv dependencies ];
+    shellHook = ''
+      alias run='iex'
+
+      nixpkgs-fmt default.nix
+    '';
+  };
+
+  package = pkgs.stdenv.mkDerivation {
+    pname = appName;
+    version = appVersion;
+
+    src = ./.;
+
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    buildInputs = [ elixirEnv ];
+
+    #TODO create matching installPhase
+    installPhase = ''
+      mkdir -p $out/bin
+    '';
+
+    meta = with pkgs.lib; {
+      description = "My Elixir application";
+      license = licenses.mit;
+      platforms = platforms.all;
+    };
+  };
+in
+{ shell = shell; package = package; }
