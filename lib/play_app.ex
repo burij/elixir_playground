4e2529273c080164459389cb0162a87dc6ac75c7
burij <michael.burij@gmail.com>
2025-10-09 07:05:15 +0200

    cleaning up hello world

--- lib/play_app.ex
+++ lib/play_app.ex
@@ -1,19 +1,9 @@
 defmodule PlayApp do
   @moduledoc """
   Documentation for `PlayApp`.
+
+  Empty modul at the moment, but start/2 needs to be defined, if the
+  application is using any persistent processses (for supervision).
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> PlayApp.hello()
-      "hello world!"
-
-  """
-  def hello do
-    hello = "hello world!"
-    hello
-  end
 end
