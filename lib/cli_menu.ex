a4c936740b79453b281e768d6c2def7b14faaeed
burij <michael.burij@gmail.com>
2025-10-12 21:13:15 +0200

    simple idea collection mechanism

--- lib/cli_menu.ex
+++ lib/cli_menu.ex
@@ -11,7 +11,7 @@ defmodule PlayApp.CliMenu do
 
     case read_input() do
       "1" ->
-        get_idea()
+        save_idea()
         menu_loop()
 
       "2" ->
@@ -28,12 +28,15 @@ defmodule PlayApp.CliMenu do
   end
 
   defp display_menu do
-    IO.puts("---------------------------------")
+    IO.puts("-----------------------------------------------------------------")
+    IO.puts("Current project ideas:")
+    IO.inspect(ideas_from_db())
+    IO.puts("-----------------------------------------------------------------")
     IO.puts("Please select an option:")
     IO.puts("1. Put a new idea out there")
     IO.puts("2. Up/Downvote an idea")
     IO.puts("q. Quit")
-    IO.puts("---------------------------------")
+    IO.puts("-----------------------------------------------------------------")
     IO.write("Enter your choice: ")
   end
 
@@ -43,10 +46,21 @@ defmodule PlayApp.CliMenu do
     |> String.downcase()
   end
 
-  defp get_idea do
+  defp save_idea do
     IO.write("Enter your idea: ")
     new_idea = IO.gets("") |> String.trim()
+    id = :crypto.strong_rand_bytes(2) |> Base.encode16(case: :lower)
+    votes = 1
+    idea = [%{id: id, content: new_idea, votes: votes}]
+    old_ideas = ideas_from_db()
+    ideas_list = idea ++ old_ideas
+    entry = :erlang.term_to_binary(ideas_list)
+    File.write!("ideas.dat", entry)
     IO.puts("Your idea '#{new_idea}' has been saved...")
-    new_idea
+  end
+
+  defp ideas_from_db do
+    loaded_data = File.read!("ideas.dat")
+    current_ideas_list = :erlang.binary_to_term(loaded_data)
   end
 end
