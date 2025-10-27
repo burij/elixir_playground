37db1cae757139aaf874a9c8d71a4b03a9c8b5ce
burij <michael.burij@gmail.com>
2025-10-27 07:31:32 +0100

    extract entitiy function

--- lib/ifc_tool.ex
+++ lib/ifc_tool.ex
@@ -6,7 +6,28 @@ defmodule IfcTool do
   defp parse_to_raw_entities(file_path) do
     File.stream!(file_path, [], :line)
     |> Stream.filter(&String.starts_with?(&1, "#"))
-    # |> Stream.flat_map(&extract_entity/1) # Process each line
+    |> Stream.flat_map(&extract_entity/1)
     |> Enum.to_list()
   end
+
+  defp extract_entity(line) do
+    regex = ~r/^#(\d+)=(\w+)\((.*)\);/
+
+    case Regex.run(regex, line, capture: :all) do
+      [_, id_str, type_str, content] ->
+        [
+          %{
+            id: String.to_integer(id_str),
+            type: type_str,
+            content: content,
+            original_line: String.trim_trailing(line)
+          }
+        ]
+
+      _ ->
+        # Ignore header lines, comments, or malformed lines
+        []
+    end
+  end
+
 end
