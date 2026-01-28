0e54fc9bd997e2857f73f726a5b2a937b00f8275
burij <michael.burij@gmail.com>
2026-01-28 07:00:18 +0100

    playing with scripting

--- script.exs
+++ script.exs
@@ -1,14 +1,35 @@
-defmodule Math do
-  def zero?(0) do
-    true
+defmodule Script do
+  def get_commands do
+    [
+    {"git", ["status"]}
+    ]
   end
 
-  def zero?(x) when is_integer(x) do
-    false
+  def execute(command_list) do
+    command_list
+    |> Enum.map(fn {cmd, args} ->
+      IO.puts("Running: #{cmd} #{Enum.join(args, " ")}")
+
+      case System.cmd(cmd, args, stderr_to_stdout: true) do
+        {output, 0} -> {:ok, output}
+        {output, exit_code} -> {:error, exit_code, output}
+      end
+    end)
   end
 end
 
-IO.puts Math.zero?(0)         #=> true
-IO.puts Math.zero?(1)         #=> false
-IO.puts Math.zero?([1, 2, 3]) #=> ** (FunctionClauseError)
-IO.puts Math.zero?(0.0)       #=> ** (FunctionClauseError)
+Script.get_commands() |> Script.execute()
+
+
+# IO.puts("Server upgrade script is launching!")
+
+# {output, _} = System.cmd("docker", ["compose", "pull"], cd: "/srv/config")
+# IO.puts(output)
+
+# {output, _} = System.cmd("docker", ["compose", "up", "-d"], cd: "/srv/config")
+# IO.puts(output)
+
+# {_, _} = System.cmd("sleep", ["20"])
+
+# {output, _} = System.cmd("docker", ["image", "prune", "-f"], cd: "/srv/config")
+# IO.puts(output)
