2e8d3a6a05a6da2c40e1520e9beb7f60fc5583be
burij <michael.burij@gmail.com>
2026-01-29 20:55:44 +0100

    using system.shell for scripting

--- script.exs
+++ script.exs
@@ -1,40 +1,28 @@
 defmodule Script do
   def run do
     IO.puts("Server upgrade script is launching!")
-    get_commands() |> execute() |> message()
+    get_commands() |> execute()
   end
 
   defp get_commands do
     [
-      {"docker", ["compose", "pull"], cd: "/srv/config"},
-      {"docker", ["compose", "up", "-d"], cd: "/srv/config"},
-      {"sleep", ["20"]},
-      {"docker", ["image", "prune", "-f"], cd: "/srv/config"}
+      "cd /srv/config",
+      "docker compose pull",
+      "docker compose up -d",
+      "sleep 20",
+      "docker image prune -f"
     ]
   end
 
-  defp execute(command_list) do
-    Enum.reduce_while(command_list, :ok, fn {cmd, args, opts}, _acc ->
-      IO.puts("Running: #{cmd} #{Enum.join(args, " ")}")
+  defp execute(commands) do
+    Enum.map(commands, fn cmd ->
+      IO.puts("> #{cmd}")
 
-      case System.cmd(cmd, args, Keyword.put_new(opts, :stderr_to_stdout, true)) do
-        {output, 0} ->
-          IO.puts(output)
-          {:cont, :ok}
-        {output, exit_code} ->
-          {:halt, {:error, exit_code, output}}
+      case System.shell(cmd, into: IO.stream(:stdio, :line)) do
+        {_, 0} -> IO.puts("ok!")
+        {_, code} -> IO.puts("something went wrong. exit code: #{code}")
       end
     end)
-  end
-
-  defp message(:ok) do
-    IO.puts("OK!")
-  end
-
-  defp message([{:error, exit_code, output}]) do
-    IO.puts("Something went wrong!")
-    IO.puts("Exit code: #{exit_code}")
-    IO.puts(output)
   end
 end
 
