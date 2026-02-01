c350ff2925944e47203f7665594ffaeb0afa3a68
burij <michael.burij@gmail.com>
2026-02-01 14:50:50 +0100

    some changes in the update script

--- script.exs
+++ script.exs
@@ -2,24 +2,24 @@ defmodule Script do
   def run do
     IO.puts("Server upgrade script is launching!")
     get_commands() |> execute()
-    # IO.puts("do not forget to start nextcloud")
-    # IO.puts("https://box:8080")
+    IO.puts("do not forget to start nextcloud")
+    IO.puts("https://box:8080")
   end
 
-  defp get_stamp do
+  defp _get_stamp do
     Date.utc_today()
     |>Date.to_string()
   end
 
   defp get_commands do
     pr_dir = "/srv/config"
-    stamp = get_stamp()
 
     [
-      "cd #{pr_dir}; docker compose pull",
-      "cd #{pr_dir}; docker compose up -d --force-recreate --remove-orphans",
-      # "sleep 20",
-      "docker image prune -af",
+      "cd #{pr_dir};sudo docker compose pull",
+      "docker images --format '{{.Repository}}:{{.Tag}}' | xargs -L1 docker pull;",
+      "cd #{pr_dir};sudo docker compose up -d --force-recreate --remove-orphans",
+      "sleep 20",
+      "sudo docker image prune -af",
     ]
   end
 
